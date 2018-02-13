58be89772851bde4d61652072dfca03d001e383b
Gabriel Robert <g.robert092@gmail.com>
2018-02-13 15:34:43 -0500

    Basic builder with ctor tests

--- src/Spk.UnhandledExceptionHandlerCore/src/Utils/ExceptionWithDataBuilder.cs
+++ src/Spk.UnhandledExceptionHandlerCore/src/Utils/ExceptionWithDataBuilder.cs
@@ -1,7 +1,17 @@
-﻿namespace Spk.UnhandledExceptionHandlerCore.Utils
+﻿using System;
+using System.Web;
+using Spk.Common.Helpers.Guard;
+
+namespace Spk.UnhandledExceptionHandlerCore.Utils
 {
     public class ExceptionWithDataBuilder
     {
-        
+        public ExceptionWithDataBuilder(
+            Exception exception,
+            HttpRequest request)
+        {
+            exception.GuardIsNotNull(nameof(exception));
+            request.GuardIsNotNull(nameof(request));
+        }
     }
 }
--- tests/Spk.Tests.UnhandledExceptionHandlerCore/Utils/ExceptionWithDataBuilderTests.cs
+++ tests/Spk.Tests.UnhandledExceptionHandlerCore/Utils/ExceptionWithDataBuilderTests.cs
@@ -1,14 +1,30 @@
-﻿using Shouldly;
+﻿using System;
+using System.Web;
+using Moq;
+using Spk.UnhandledExceptionHandlerCore.Utils;
 using Xunit;
 
 namespace Spk.Tests.UnhandledExceptionHandlerCore.Utils
 {
     public class ExceptionWithDataBuilderTests
     {
-        [Fact]
-        public void Test_ShouldPass()
+        private HttpRequest _request;
+
+        public ExceptionWithDataBuilderTests()
         {
-            true.ShouldBeTrue();
+            _request = new HttpRequest("", "http://localhost", "");
+        }
+
+        [Fact]
+        public void ctor_ShouldThrow_WhenExceptionIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ExceptionWithDataBuilder(null, _request));
+        }
+
+        [Fact]
+        public void ctor_ShouldThrow_WhenRequestIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ExceptionWithDataBuilder(new Exception(), null));
         }
     }
 }
