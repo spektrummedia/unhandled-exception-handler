275eead456bba9b8ab6fc3df8b384a9c143214e2
Mathieu St-Gelais <mstgelais@spektrummedia.com>
2018-02-13 10:41:11 -0500

    Added Ui projet v.1.2.4. Removed several default values from web.config.install.

---
+++ src/Spk.UnhandledExceptionHandlerUi/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Unhandled Exception Handler UI")]
+[assembly: AssemblyDescription("Manages unhandled exceptions thrown in an application by adding logging and sending emails.")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b0eb1583-9b72-405f-807b-5cf4aa332663")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Spk.UnhandledExceptionHandlerUi/content/App_Start/FilterConfig.cs.pp
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using Spk.UnhandledExceptionHandlerCore.Filters;
+
+namespace $rootnamespace$
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HttpNotFoundFilter { Order = 1 });
+        }
+    }
+}
